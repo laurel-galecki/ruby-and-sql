14996ca6556399ab7a2e2d20eb4b01fd1548bda6
Laurel Galecki <laurel.galecki@kellogg.northwestern.edu>
2023-04-27 02:10:19 +0000

    end of class

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -11,7 +11,38 @@ Contact.destroy_all
 # - Insert and read contact data for companies in the database
 
 # 1. insert new rows in the contacts table with relationship to a company
+puts "Companies: #{Company.all.count}"
+puts "Contacts: #{Contact.all.count}"
+
+apple = Company.find_by({"name"=>"Apple"})
+
+cook = Contact.new
+cook["first_name"] = "Tim"
+cook["last_name"] = "Cook"
+cook["email"] = "tim@apple.com"
+cook["company_id"] = apple["id"]
+cook.save
+
+puts cook.inspect
+
+craig = Contact.new
+craig["first_name"] = "Craig"
+craig["last_name"] = "Fedrighi"
+craig["email"] = "craig@apple.com"
+craig["company_id"] = apple["id"]
+craig.save
+
+puts craig.inspect
 
 # 2. How many contacts work at Apple?
 
+apple_contacts = Contact.where({"company_id"=>apple["id"]})
+puts "Apple Contacts: #{apple_contacts.count}"
+
 # 3. What is the full name of each contact who works at Apple?
+
+for contact in apple_contacts
+    first_name = contact["first_name"]
+    last_name = contact["last_name"]
+    puts "#{first_name} #{last_name}"
+end
--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,15 +21,44 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+employee1 = Salesperson.new
+employee1["first_name"] = "Bob"
+employee1["last_name"] = "Smith"
+employee1["email"] = "bobsmith@gmail.com"
+employee1.save
+
+employee2 = Salesperson.new
+employee2["first_name"] = "Jane"
+employee2["last_name"] = "Jones"
+employee2["email"] = "janejones@gmail.com"
+employee2.save
+
 # 3. write code to display how many salespeople rows are in the database
+
+puts "There are #{Salesperson.all.count} salespeople"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
 
+bob = Salesperson.find_by({"first_name"=>"Bob","last_name"=>"Smith"})
+bob["email"] = "bsmith@gmail.com"
+bob.save
+
+puts bob["email"]
+
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+
+#use loop
+all_employees = Salesperson.all
+
+for employee in all_employees
+    first_name = employee["first_name"]
+    last_name = employee["last_name"]
+    puts "#{first_name} #{last_name}"
+end
 
 # ---------------------------------
 # Salespeople: 2
